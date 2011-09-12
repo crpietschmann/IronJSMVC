dde1bdff0edd77c962e5b325f959aad6e2a743da
Chris Pietschmann <chris@pietschsoft.com>
2011-09-12 13:57:44 -0500

    updated license

--- IronJSMVC/Properties/AssemblyInfo.cs
+++ IronJSMVC/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IronJSMVC")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/crpietschmann/IronJSMVC")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Chris Pietschmann")]
 [assembly: AssemblyProduct("IronJSMVC")]
